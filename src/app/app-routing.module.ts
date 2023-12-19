86bac9656ce641b72fd3c04c91e15377a0a6f7e7
João Marcelo <joaomarcelo588@gmail.com>
2023-12-19 12:32:22 -0300

    Criado componente do menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+const SGC = "sgc/";
+
 const routes: Routes = [
+
   {
     path: '',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
   },
+  {
+    path: SGC + 'menus',
+    loadChildren: () => import('./modules/menu/menu.module').then(m => m.MenuModule)
+  }
 ];
 
 @NgModule({
